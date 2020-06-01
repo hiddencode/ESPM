314f9a44c824ec3e5a277030e54bb8d446f9372b
hiddencode <459030@mail.ru>
2020-06-01 06:25:36 +0300

    Added adversting structure

--- test.c
+++ test.c
@@ -352,6 +352,40 @@ static esp_ble_adv_params_t test_adv_params = {
 	.adv_filter_policy	= ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
 };
 
+/**
+ * Common GATT profile handler
+ * Store the generated interface,
+ * correspondig profile event handler
+ * @param event 	- External event
+ * @param param 	- Pointer to parameters for GATT
+ */
+static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
+	       			esp_ble_gatts_cb_param_t * param)
+{
+	// If event is register event, store tthe gatts_if for each profile
+	if(event == ESP_GATTS_REG_EVT){
+		if(param->reg,status == ESP_GATT_OK){
+			gl_profile_tab[param->reg.app_id].gatts_if = gatts_if;
+		}else{
+			ESP_LOGI(GATTS_TAG, "Reg app failed, app_id %04x status %d\n", param->reg.app_id, param->reg.status);
+			return;
+		}
+	}
+
+	// If the gatts_if equal to profile A, call profile A cb handler,
+	// so here call each profile`s callback
+	do{
+		int idx;
+		for(idx = 0; idx < PROFILE_NUM; idx++){
+			if(gatts_if == ESP_GATT_IF_NONE || gatts_if == gl_profile_tab[idx].gatts_if){
+				if(gl_profile_tab[idx].gatts_cb){
+					gl_profile_tab[idx].gatts_cb(event, gatts_if, param);
+				}
+			}
+		}
+
+	}while(0);
+}
 
 
 /* Entry point of project */
